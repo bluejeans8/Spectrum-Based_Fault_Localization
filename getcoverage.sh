ccdb7fd0ecec5f718233e40baa054bb53b2360ab
bluejeans98 <jsk0821@kaist.ac.kr>
2022-07-19 12:45:17 -0700

    modified directory structure

--- getcoverage.sh
+++ getcoverage.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-cnt=0
+# cnt=0
 while read line ; do
-    cnt=$(($cnt + 1))
-    python lang_get_tmp_classes.py $line
-    ret=$?
-    if [ $ret -gt 0 ]
-    then
-        echo $cnt "pass" $ret $line
-    else
-        echo "fail"
-        exit 0
-    fi
-    # defects4j coverage -w "../../tmp/lang_1_fixed" -t $line -i lang_tmp_classes
-    # cp ../../tmp/lang_1_fixed/coverage.xml coverage_data/$line.xml
-done < parsed_lang_all_tests
+    # cnt=$(($cnt + 1))
+    # python lang_get_tmp_classes.py $line
+    # ret=$?
+    # if [ $ret -gt 0 ]
+    # then
+    #     echo $cnt "pass" $ret $line
+    # else
+    #     echo "fail"
+    #     exit 0
+    # fi
+    echo "\n[Testing] $line"
+    defects4j coverage -w "../../tmp/lang_1_fixed" -t $line -i "lang_classes/lang_all_classes"
+    cp ../../tmp/lang_1_fixed/coverage.xml coverage_data/$line.xml
+done < "lang_tests/parsed_lang_all_tests"
