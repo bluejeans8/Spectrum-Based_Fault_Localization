68e389d84d5d736845dad2ae9dadb8a3e26f3f47
bluejeans98 <jsk0821@kaist.ac.kr>
2022-08-15 02:26:24 -0700

    get bugs location in lang repository

--- d4j_bugs_parser.py
+++ d4j_bugs_parser.py
@@ -1,8 +1,14 @@
 import json
 
 count = 0
+x = []
 with open('defects4j-bugs.json', 'r') as rf:
     data = json.load(rf)
     for version in data:
         if version["project"] == "Lang":
-            print(version["bugId"], version["changedFiles"])
+            x.append(version)
+x.sort(key=lambda x: x.get('bugId'))
+bugs_json = json.dumps(x, indent=4)
+
+with open("lang_bugs.json","w") as wf:
+    wf.write(bugs_json)
